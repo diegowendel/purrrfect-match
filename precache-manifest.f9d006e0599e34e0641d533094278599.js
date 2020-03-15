a2703aebc15c95e27f02421d3047ae624a14fd3e
Diego Wendel <diegowendelf@gmail.com>
2020-03-15 19:27:45 -0300

    Updates

---
+++ precache-manifest.f9d006e0599e34e0641d533094278599.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c3b556ab5cd7ddcf69c5c7e02446e8",
+    "url": "/purrrfect-match/index.html"
+  },
+  {
+    "revision": "bdde44c2c56ad900ab0b",
+    "url": "/purrrfect-match/static/css/main.42b4ce21.chunk.css"
+  },
+  {
+    "revision": "b2eec9ddb1e6ed494ebd",
+    "url": "/purrrfect-match/static/js/2.8d3b92cc.chunk.js"
+  },
+  {
+    "revision": "7f7b7554ad8c1d8206ae278b825bcae0",
+    "url": "/purrrfect-match/static/js/2.8d3b92cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdde44c2c56ad900ab0b",
+    "url": "/purrrfect-match/static/js/main.a136408a.chunk.js"
+  },
+  {
+    "revision": "a8ec607ca48bd706d6bd",
+    "url": "/purrrfect-match/static/js/runtime-main.cc108dd9.js"
+  },
+  {
+    "revision": "671bec606bdc2c51dc6d50aec7cb4507",
+    "url": "/purrrfect-match/static/media/Brown-Bold.671bec60.ttf"
+  },
+  {
+    "revision": "645ac9beaddc55186b6e22927bf38a86",
+    "url": "/purrrfect-match/static/media/Brown-Italic.645ac9be.ttf"
+  },
+  {
+    "revision": "03efe788303ee4e7ec0b0db970540b95",
+    "url": "/purrrfect-match/static/media/BrownMedium-Regular.03efe788.ttf"
+  },
+  {
+    "revision": "f4f7d91e3620059cb7e5aa9bc7386e31",
+    "url": "/purrrfect-match/static/media/arrow_next.f4f7d91e.svg"
+  },
+  {
+    "revision": "212691ef7599f563f39d1559b6b660f1",
+    "url": "/purrrfect-match/static/media/arrow_next_white.212691ef.svg"
+  },
+  {
+    "revision": "ae64880c4277421488d942aaba7edb18",
+    "url": "/purrrfect-match/static/media/banner.ae64880c.jpg"
+  },
+  {
+    "revision": "504aec2f65fe2717ee87d66eb23fc1dd",
+    "url": "/purrrfect-match/static/media/cat_loading.504aec2f.svg"
+  },
+  {
+    "revision": "91c04bd87354c2b3c9a51643c39ffcbd",
+    "url": "/purrrfect-match/static/media/plus.91c04bd8.svg"
+  },
+  {
+    "revision": "24fe4a7dd9c1f5d86c0ad80e84950960",
+    "url": "/purrrfect-match/static/media/plus_white.24fe4a7d.svg"
+  },
+  {
+    "revision": "049ff919d92bd00623f98513fa781236",
+    "url": "/purrrfect-match/static/media/purrrfect_logo.049ff919.svg"
+  },
+  {
+    "revision": "aeb25135c5a756e65dca074a53802495",
+    "url": "/purrrfect-match/static/media/search.aeb25135.svg"
+  }
+]);
